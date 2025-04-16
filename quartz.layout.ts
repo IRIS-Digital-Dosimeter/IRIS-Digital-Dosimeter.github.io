cf9c0190a3866a2f5b4aafe43b70576bfdcfa210
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2025-04-16 12:50:35 -0700

    adding giscus support

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'IRIS-Digital-Dosimeter/IRIS-Digital-Dosimeter.github.io',
+        // from data-repo-id
+        repoId: 'R_kgDOOZ2UOQ',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOOZ2UOc4CpK08',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
